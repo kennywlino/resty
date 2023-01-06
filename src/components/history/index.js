365a5a6239b418f26cb04a01a94f6f6fb9db923a
Kenny W. Lino <kennywlino@gmail.com>
2023-01-05 18:10:32 -0800

    Added pretty-print-json package to display JSON data better.

--- src/components/history/index.js
+++ src/components/history/index.js
@@ -4,10 +4,10 @@ const HistoryElement = (props) => {
     // data =;
     return (
         <>
-        <h3>URL: {props.data.url}</h3>
-        <p>METHOD: {props.data.method}</p>
-        <p>REQUEST DATA: {props.data}</p>
-        <p>RETRIEVED DATA:</p>
+        <h3>URL: {props.item.url}</h3>
+        <p>METHOD: {props.item.method}</p>
+        <p>REQUEST DATA: {props.item.reqData}</p>
+        <p>RETRIEVED DATA: {props.item.data}</p>
         </>
     )
 }
@@ -15,11 +15,12 @@ const HistoryElement = (props) => {
 const History = (props) => {
 
     let apiHistoryElements = props.apiHistory.map(item => {
-        return <HistoryElement data={item}/>
+        return <HistoryElement item={item}/>
     });
 
     return (
         <>
+        {apiHistoryElements}
         </>
     )
 }
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,8 +1,11 @@
+import { prettyPrintJson } from "pretty-print-json";
+import React from "react";
+
 const Results = (props) => {
   return (
     <section>
-     {props.loading ? <pre>loading...</pre> : <pre data-testid="results">{props.data ? JSON.stringify(props.data, undefined, 2) : null}
-     </pre> }
+     {props.loading ? <pre>loading...</pre> : 
+     props.data ? <pre data-testid="results" dangerouslySetInnerHTML={{ __html: prettyPrintJson.toHtml(props.data) }} /> : null}
     </section>
   ) 
 }
